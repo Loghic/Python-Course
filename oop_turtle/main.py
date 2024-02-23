85d2d704b9305d8c2bb289b9afb68fe86f0c257d
Loghic <michalek.matej.loghin@gmail.com>
2024-02-23 17:19:24 +0100

    oop_turtle and prettyTable

--- oop_turtle/main.py
+++ oop_turtle/main.py
@@ -1,11 +1,32 @@
 # import turtle
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+# table.field_names = ["Pokemon Name", "Type"]
+# table.add_rows(
+#     [
+#     ["Pikachu", "Electric"],
+#     ["Squirtle", "Water"],
+#     ["Charmander", "Fire"]
+#     ]
+# )
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'
+
+print(table)
+
